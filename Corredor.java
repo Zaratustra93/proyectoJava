4de10d708b89b9a89abef6b8e436dafbc64a59b0
akeenDeveloper <joaquin1208@me.com>
2013-11-15 13:42:53 +0100

    Proyecto java corredores
    Java project

---
+++ Corredor.java
@@ -0,0 +1,48 @@
+public class Corredor {
+
+	
+	private String sexo;
+	private String nombre;
+	private String fecha_de_nacimiento;
+	
+	
+	
+	public Corredor(String sexo,String nombre,String fecha_de_nacimiento)
+	{
+		
+		this.sexo=sexo;
+		this.nombre=nombre;
+		this.fecha_de_nacimiento=fecha_de_nacimiento;
+		
+	}
+	
+	public void setSexo(String sexo)
+	{
+		
+		this.sexo=sexo;
+		
+	}
+	public String getSexo()
+	{
+		
+		return this.sexo;
+		
+	}
+	
+	public void setNombre(String nombre)
+	{
+		
+		this.nombre=nombre;
+		
+	}
+	public String getNombre()
+	{
+		
+		return this.nombre;
+		
+	}
+	
+
+	
+	
+}
